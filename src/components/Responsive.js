3828005ef235955e040b26f2e81c78e55718909d
이경은 <2kyung19@gmail.com>
2022-01-12 12:03:11 +0900

    feat : react responsive [#1]

---
+++ src/components/Responsive.js
@@ -0,0 +1,23 @@
+import { useMediaQuery } from "react-responsive";
+
+const LaptopL = ({ children }) => {
+  const view = useMediaQuery({ minWidth: 1200 });
+  return view ? children : null;
+};
+
+const LaptopS = ({ children }) => {
+  const view = useMediaQuery({ minWidth: 992, maxWidth: 1199 });
+  return view ? children : null;
+};
+
+const Tablet = ({ children }) => {
+  const view = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  return view ? children : null;
+};
+
+const Mobile = ({ children }) => {
+  const view = useMediaQuery({ maxWidth: 767 });
+  return view ? children : null;
+};
+
+export { LaptopL, LaptopS, Tablet, Mobile };

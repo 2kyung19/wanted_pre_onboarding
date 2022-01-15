142c31a3ea0c9b6882fface7296120c1e7d36bb8
이경은 <2kyung19@gmail.com>
2022-01-15 19:16:40 +0900

    feat : carousel animation [#3]

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,82 +1,226 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
 import styled from "styled-components";
 import { CarouselL, CarouselS } from "./Responsive";
-import Swipe from "./Swipe";
-
-import slide1 from "../images/slide/slide_1.jpeg";
-import slide2 from "../images/slide/slide_2.jpeg";
-import slide3 from "../images/slide/slide_3.jpeg";
-import slide4 from "../images/slide/slide_4.jpeg";
-import slide5 from "../images/slide/slide_5.jpeg";
-import slide6 from "../images/slide/slide_6.jpeg";
-import slide7 from "../images/slide/slide_7.jpeg";
-import slide8 from "../images/slide/slide_8.jpeg";
-import slide9 from "../images/slide/slide_9.jpeg";
-import slide10 from "../images/slide/slide_10.jpeg";
-import slide11 from "../images/slide/slide_11.jpeg";
+import { slideList } from "./SlideList";
 
 const Container = styled.div`
   width: 100%;
 `;
 
 const Slide = styled.div`
-  width: ${(props) => props.width || `${1060 * 13}px`};
+  width: ${(props) => props.width || `${1060 * 8 + 160}px`};
   display: flex;
   overflow: hidden;
+  transition: ${(props) => props.animation || "transform 400ms"};
+  transform: translateX(-${(props) => props.transform}px);
 `;
 
-const Img = styled.div`
+const Box = styled.div`
   position: relative;
   width: ${(props) => props.width || "1060px"};
   height: ${(props) => props.height || 300}px;
-  border-radius: 4px;
-  background-image: url("${(props) => props.url}");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
   margin-left: 10px;
   margin-right: 10px;
 `;
 
+const Img = styled.div`
+  width: 100%;
+  height: ${(props) => props.height || 300}px;
+  border-radius: 4px;
+  background-image: url("${(props) => props.src}");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const Description = styled.div`
+  position: absolute;
+  width: 330px;
+  height: 146px;
+  bottom: 28px;
+  left: 34px;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
+
+const Hr = styled.hr`
+  border: 0px;
+  border-top: 1px solid #ececec;
+  padding: 0px;
+  margin: 0px;
+`;
+
+const H2 = styled.div`
+  height: ${(props) => props.height || "auto"};
+  margin-top: ${(props) => props.top || 0}px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding: 0px;
+  text-align: ${(props) => props.align || "center"};
+  font-size: ${(props) => props.font || 18}px;
+  font-weight: ${(props) => props.weight || "normal"};
+  font-family: sans-serif;
+  color: ${(props) => props.color || "#333333"};
+  line-height: ${(props) => props.line + props.font || props.font}px;
+`;
+
+const Link = styled.div`
+  text-align: ${(props) => props.align || "center"};
+  margin: ${(props) => props.margintb || 12}px;
+  margin-left: ${(props) => props.marginlr || 20}px;
+  margin-right: ${(props) => props.marginlr || 20}px;
+  font-weight: bold;
+  font-size: 14px;
+  color: #3366ff;
+`;
+
+const useWindowSize = () => {
+  const [size, setSize] = useState(0);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize(window.innerWidth);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+};
+
 const Carousel = () => {
+  const width = useWindowSize();
+  const [index, setIndex] = useState(1);
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+  const [animation, setAnimation] = useState("transform 500ms");
+  const slideS = useRef(null);
+  const slideL = useRef(null);
+  const [smallSlide, setSmallSlide] = useState(0);
+  const [largeSlide, setLargeSlide] = useState(0);
+
+  const slideLeft = () => {
+    if (index === 1) {
+      setAnimation("none");
+      setIndex(6);
+    } else {
+      setAnimation("transform 500ms");
+      setIndex(index - 1);
+    }
+  };
+
+  const slideRight = () => {
+    if (index === 7) {
+      setAnimation("none");
+      setIndex(1);
+    } else {
+      setAnimation("transform 500ms");
+      setIndex(index + 1);
+    }
+  };
+
+  const handleTouchStart = (e) => {
+    setTouchStart(e.touches ? e.touches[0].clientX : e.clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEnd(e.touches ? e.touches[0].clientX : e.clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStart - touchEnd > 70) {
+      slideRight();
+    }
+
+    if (touchStart - touchEnd < -70) {
+      slideLeft();
+    }
+  };
+
+  useEffect(() => {
+    if (slideS.current !== null) {
+      setSmallSlide(slideS.current.offsetWidth / 8);
+    }
+    if (slideL.current !== null) {
+      setLargeSlide(slideL.current.offsetWidth / 8);
+    }
+    console.log(index, width);
+  }, [index, width]);
+
+  useEffect(() => {
+    const loop = setInterval(() => {
+      slideRight();
+    }, 3000);
+
+    return () => clearInterval(loop);
+  }, [index]);
+
   return (
     <Container>
-      <Swipe>
+      <div
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        onMouseDown={handleTouchStart}
+        onMouseMove={handleTouchMove}
+        onMouseUp={handleTouchEnd}
+      >
         <CarouselS>
-          <Slide width="calc(1300% - 1040px)">
-            <Img url={slide11} width="calc(100%/13)" height={183} />
-            <Img url={slide1} width="calc(100%/13)" height={183} />
-            <Img url={slide2} width="calc(100%/13)" height={183} />
-            <Img url={slide3} width="calc(100%/13)" height={183} />
-            <Img url={slide4} width="calc(100%/13)" height={183} />
-            <Img url={slide5} width="calc(100%/13)" height={183} />
-            <Img url={slide6} width="calc(100%/13)" height={183} />
-            <Img url={slide7} width="calc(100%/13)" height={183} />
-            <Img url={slide8} width="calc(100%/13)" height={183} />
-            <Img url={slide9} width="calc(100%/13)" height={183} />
-            <Img url={slide10} width="calc(100%/13)" height={183} />
-            <Img url={slide11} width="calc(100%/13)" height={183} />
-            <Img url={slide1} width="calc(100%/13)" height={183} />
+          <Slide
+            width="calc(800% - 800px + 160px)"
+            transform={smallSlide * index - 40}
+            animation={animation}
+            ref={slideS}
+          >
+            {slideList.map((s, index) => (
+              <Box key={index} width="calc(100%/8)" height={281.94}>
+                <Img src={s.src} height={183} />
+                <H2 top={20} weight="bold">
+                  {s.title}
+                </H2>
+                <H2 top={6} font={14} color="#666666">
+                  {s.sub}
+                </H2>
+                <Link>
+                  <span>바로가기 </span>
+                  <span>{`>`}</span>
+                </Link>
+              </Box>
+            ))}
           </Slide>
         </CarouselS>
         <CarouselL>
-          <Slide>
-            <Img url={slide11} />
-            <Img url={slide1} />
-            <Img url={slide2} />
-            <Img url={slide3} />
-            <Img url={slide4} />
-            <Img url={slide5} />
-            <Img url={slide6} />
-            <Img url={slide7} />
-            <Img url={slide8} />
-            <Img url={slide9} />
-            <Img url={slide10} />
-            <Img url={slide11} />
-            <Img url={slide1} />
+          <Slide
+            transform={largeSlide * index - (width - 1060) / 2}
+            animation={animation}
+            ref={slideL}
+          >
+            {slideList.map((s, index) => (
+              <Box key={index} height={300}>
+                <Img src={s.src} />
+                <Description>
+                  <H2 font={20} line={10} top={20} align="left" weight="bold">
+                    {s.title}
+                  </H2>
+                  <H2
+                    font={14}
+                    line={10}
+                    height="44px"
+                    align="left"
+                    color="#333333"
+                  >
+                    {s.sub}
+                  </H2>
+                  <Hr />
+                  <Link marginlr={21} margintb={16} align="left">
+                    <span>바로가기 </span>
+                    <span>{`>`}</span>
+                  </Link>
+                </Description>
+              </Box>
+            ))}
           </Slide>
         </CarouselL>
-      </Swipe>
+      </div>
     </Container>
   );
 };
---
+++ src/components/SlideList.js
@@ -0,0 +1,51 @@
+import slide1 from "../images/slide/slide_1.jpeg";
+import slide2 from "../images/slide/slide_2.jpeg";
+import slide3 from "../images/slide/slide_3.jpeg";
+import slide4 from "../images/slide/slide_4.jpeg";
+import slide5 from "../images/slide/slide_5.jpeg";
+import slide6 from "../images/slide/slide_6.jpeg";
+
+const slideList = [
+  {
+    src: slide6,
+    title: "포트폴리오를 부탁해!",
+    sub: "디자이너의 포폴 살펴보기",
+  },
+  {
+    src: slide1,
+    title: "개발자 되고싶은 분들!?",
+    sub: "프론트엔드 무료 교육과정 참여하기",
+  },
+  {
+    src: slide2,
+    title: "마케팅 주니어를 찾습니다",
+    sub: "1월 17일(월) 설명회 신청하기",
+  },
+  {
+    src: slide3,
+    title: "해, 커리어 EP 02 공개",
+    sub: "마지막 관문 2라운드의 승자는?",
+  },
+  {
+    src: slide4,
+    title: "성과를 내는 마케팅",
+    sub: "실제 사례를 공개합니다!",
+  },
+  {
+    src: slide5,
+    title: "성장하는 개발자가 되려면?",
+    sub: "OOO 검색하지 말 것!",
+  },
+  {
+    src: slide6,
+    title: "포트폴리오를 부탁해!",
+    sub: "디자이너의 포폴 살펴보기",
+  },
+  {
+    src: slide1,
+    title: "개발자 되고싶은 분들!?",
+    sub: "프론트엔드 무료 교육과정 참여하기",
+  },
+];
+
+export { slideList };
